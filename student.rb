026cbe0e9482ecccd3325687d1388bd143c058a1
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-06-19 14:57:07 +0100

    removed the teacher class

--- student.rb
+++ student.rb
@@ -12,16 +12,3 @@ class Student < Person
     '¯\(ツ)/¯'
   end
 end
-
-class Teacher < Person
-  attr_accessor :specialization
-
-  def initialize(id, name = 'Unknown', age = 0, specialization:, parent_permission: true)
-    super(id, name, age, parent_permission: parent_permission)
-    @specialization = specialization
-  end
-
-  def can_use_services?
-    true
-  end
-end
