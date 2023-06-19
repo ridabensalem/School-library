88a44dce170a626f3fc0d5f21a940c082f5e300b
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-06-19 12:09:39 +0100

    Create class and methods of person

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  attr_accessor :id, :name, :age
+
+  def initialize(id, name = 'Unknown', age = 0, parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
