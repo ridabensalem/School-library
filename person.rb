fb5e55a66d78cd7f8e5733e30d6daa66e96c177f
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-06-20 12:24:58 +0100

    Inherit form nameable

--- person.rb
+++ person.rb
@@ -1,12 +1,21 @@
-class Person
+require_relative 'nameable'
+# Make sure that your Person class inherits from Nameable
+# Make sure that this class has a method correct_name implemented. It should simply return the name attribute.
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
