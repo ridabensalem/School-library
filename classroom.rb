f3e539b24790f0460f17f871c3dff57de6a0d63c
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-06-21 11:16:47 +0100

    Add blong to relation to the classroom class

--- classroom.rb
+++ classroom.rb
@@ -6,6 +6,11 @@ class Classroom
     @student = []
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
   def students
     Student.all.select do |student|
       student.classroom == self
