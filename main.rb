0086650d569ba25067f93b3d6b1b94539df74459
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-06-22 10:45:22 +0100

    Create the main function

---
+++ main.rb
@@ -0,0 +1,46 @@
+require_relative 'app'
+
+def display_options
+  puts 'Choose an option:'
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List rentals for a person'
+  puts '7. Quit'
+end
+
+def process_option(app, option)
+  actions = {
+    1 => -> { app.list_books },
+    2 => -> { app.list_people },
+    3 => -> { app.create_person },
+    4 => -> { app.create_book },
+    5 => -> { app.create_rental },
+    6 => -> { app.list_rentals_for_person },
+    7 => -> { puts 'Exiting the app...' }
+  }
+
+  action = actions[option]
+  if action
+    action.call
+    option != 7
+  else
+    puts 'Invalid option. Please try again.'
+    true
+  end
+end
+
+def main
+  app = App.new
+
+  loop do
+    display_options
+    option = gets.chomp.to_i
+    break unless process_option(app, option)
+  end
+end
+
+# Invoke the entry point method
+main
