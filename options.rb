8053cda0878780baae10968ac7ffe8d7e308a0f6
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-06-26 10:52:16 +0100

    Sperate the display list to another class

---
+++ options.rb
@@ -0,0 +1,12 @@
+class Options
+  def display_options
+    puts 'Choose an option:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List rentals for a person'
+    puts '7. Quit'
+  end
+end
