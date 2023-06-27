9dcaaf64a5357a7cc8b497627b0baffbff0feb2b
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-06-27 15:29:55 +0100

    add methodes for preserving data

--- main.rb
+++ main.rb
@@ -1,30 +1,15 @@
 require_relative 'app'
 require_relative 'options'
-require 'json'
 
 def main
   options = Options.new
   app = App.new
 
-  # getting the user input
-  user_input = -> { gets.chomp.to_i }
-
   loop do
     options.display_options
-    option = user_input.call
-
-    case option
-    when 7
-      app.save_data('data.json')
-      puts 'Exiting the app...'
-      break
-    when 8
-      app.load_data('data.json')
-    else
-      break unless options.process_option(app, option)
-    end
+    option = gets.chomp.to_i
+    break unless options.process_option(app, option)
   end
 end
 
-# Invoke the entry point method
 main
